889aff5fd70afffacff013cd1a08868fbdc53a5c
slyguy5646 <lpmonaghan06@gmail.com>
2026-01-30 11:45:25 -0500

    hono

---
+++ apps/api/src/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/hello", (c) => {
+  return c.json({
+    ok: true,
+    message: "Hello Hono!",
+  });
+});
+
+export default {
+  port: 3001,
+  fetch: app.fetch,
+};
